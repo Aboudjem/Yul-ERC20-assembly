06e967c9546e1092acc87a754d4a85e147e5a9eb
Adam Boudj <boudjemaa.adam@gmail.com>
2023-04-28 19:08:28 +0400

    :white_check_mark: add tests

--- test/YulERC20.t.sol
+++ test/YulERC20.t.sol
@@ -7,7 +7,10 @@ import "../src/YulERC20.sol";
 contract YulERC20Test is Test {
     YulERC20 public token;
 
+    address owner;
+
     function setUp() public {
+        vm.prank(owner);
         token = new YulERC20();
     }
 
@@ -15,4 +18,28 @@ contract YulERC20Test is Test {
         assertEq(token.name(), "Adam");
     }
 
+    function testSymbol() public {
+        assertEq(token.symbol(), "ADX");
+    }
+
+    function testDecimals() public {
+        assertEq(token.decimals(), 18);
+    }
+
+    function testBalanceOf(address) public {
+        assertEq(token.balanceOf(owner), 10000);
+    }
+
+
+    function testTransfer(address acc) public {
+
+        assertEq(token.balanceOf(owner), 10000);
+        vm.prank(owner);
+        vm.assume(acc != owner);
+        assertEq(token.transfer(acc, 100), true);
+
+        assertEq(token.balanceOf(owner), 9900);
+
+//        assertEq(token.transfer(acc, amount), true);
+    }
 }
