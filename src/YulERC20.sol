120f013a6f02aa74a084b7bd9cc787fa770833e6
Adam Boudj <boudjemaa.adam@gmail.com>
2023-04-28 23:14:00 +0400

    :sparkles: natspec + guide comments

--- src/YulERC20.sol
+++ src/YulERC20.sol
@@ -39,28 +39,50 @@ contract YulERC20 {
 
     }
 
+    /// @notice Returns the name of the token.
+    /// @return The name of the token as a string.
     function name() public pure returns(string memory) {
         assembly {
+            // Get the free memory pointer
             let memptr := mload(0x40)
+
+            // Store the length of the name
             mstore(memptr, 0x20)
+
+            // Store the name length
             mstore(add(memptr, 0x20), nameLength)
+
+            // Store the name data
             mstore(add(memptr, 0x40), nameData)
 
+            // Return the memory containing the name
             return(memptr, 0x60)
         }
     }
 
+    /// @notice Returns the symbol of the token.
+    /// @return The symbol of the token as a string.
     function symbol() public pure returns(string memory) {
         assembly {
+            // Get the free memory pointer
             let memptr := mload(0x40)
+
+            // Store the length of the symbol
             mstore(memptr, 0x20)
+
+            // Store the symbol length
             mstore(add(memptr, 0x20), symbolLength)
+
+            // Store the symbol data
             mstore(add(memptr, 0x40), symbolData)
 
+            // Return the memory containing the symbol
             return(memptr, 0x60)
         }
     }
 
+    /// @notice Returns the number of decimals for the token.
+    /// @return The number of decimals as a uint8.
     function decimals() public pure returns(uint8) {
         assembly {
             mstore(0, 18)
@@ -70,47 +92,73 @@ contract YulERC20 {
 
     function balanceOf(address) public view returns(uint256) {
         assembly {
+            // Load the input address into memory
             mstore(0x00, calldataload(0x04))
+            // Set the second memory word to zero
             mstore(0x20, 0x00)
+            // Load the balance from storage using the hash of slot and the input address
             mstore(0x00, sload(keccak256(0x00, 0x40)))
+            // Return the memory containing the balance
             return(0x00, 0x20)
         }
     }
 
+    /// @notice Transfers tokens from the caller to the specified address.
+    /// @return A boolean indicating whether the transfer was successful.
     function transfer(address, uint256) public returns(bool) {
         assembly {
+        // Get the free memory pointer
             let memptr := mload(0x40)
+        // Store the caller's address in memory
             mstore(memptr, caller())
+        // Set the second memory word to zero
             mstore(add(memptr, 0x20), 0x00)
 
+        // Calculate the storage slot for the caller's balance
             let callerBalanceSlot := keccak256(memptr, 0x40)
+        // Load the caller's balance from storage
             let callerBalance := sload(callerBalanceSlot)
 
+        // Load the input transfer value
             let value := calldataload(0x24)
-            let receiver := calldataload(0x04)
+        // Load the input receiver address
+        let receiver := calldataload(0x04)
             if lt(callerBalance, value) {
-                revert(0x00, 0x00) }
+            // Revert if the caller's balance is less than the transfer value
+                revert(0x00, 0x00)
+            }
 
+        // Calculate the new caller balance
             let newCallerBalance := sub(callerBalance, value)
+        // Store the new caller balance
             sstore(callerBalanceSlot, newCallerBalance)
 
+        // Store the receiver address in memory
             mstore(memptr, receiver)
+        // Set the second memory word to zero
             mstore(add(memptr, 0x20), 0x00)
 
-
+        // Calculate the storage slot for the receiver's balance
             let receiverBalanceSlot := keccak256(memptr, 0x40)
+        // Load the receiver's balance from storage
             let receiverBalance := sload(receiverBalanceSlot)
 
+        // Calculate the new receiver balance
             let newReceiverBalance := add(receiverBalance, value)
-
+        // Store the new receiver balance
             sstore(receiverBalanceSlot, newReceiverBalance)
 
+            // Store the transfer value in memory
             mstore(0x00, value)
-            log3(0x00, 0x20, transferHash, caller(), receiver)
+
+        // Log the transfer event with the transfer hash, caller address, and receiver address
+        log3(0x00, 0x20, transferHash, caller(), receiver)
 
 
-
+        // Store a "true" value in the first memory word
             mstore(0x00, 0x01)
+
+        // Return the memory containing "true" (0x01) value, indicating a successful transfer
             return(0x00,0x20)
         }
     }
